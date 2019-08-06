6a4100b036ac479febd10bee77d488ccf555095a
erik <erik@192.168.0.58>
2019-08-06 11:52:04 +0200

    added javadoc cleanup

--- src/openhab2/Rules.java
+++ src/openhab2/Rules.java
@@ -16,14 +16,20 @@ import java.util.Set;
 import java.util.stream.Stream;
 
 /**
+ * Represents collection of translation rules 
  *
  * @author erik
  */
 public class Rules {
 
+    /** Holds collection of rules */
     private Map<Alias, Translation> map = new LinkedHashMap<>();    //LinkedHashMap to preserve order
-    private int loadedLines;
 
+    /**
+     * Adds a translation rule to collection
+     * @param line
+     * @throws IllegalArgumentException when illegal rule definition
+     */
     public void add(String line) {
 
         //ReDo this part with sensible exception(s)
@@ -50,50 +56,51 @@ public class Rules {
         }
     }
 
-    protected Translation put(Alias alias, Translation translation) {
+    /**
+     * Directly adds translation rule to collection
+     * @param alias key
+     * @param translation value
+     * @exception IllegalArgumentException (unchecked) when duplicate alias
+     */
+    protected void put(Alias alias, Translation translation) {
         Translation rv = map.put(alias, translation);
         if (rv != null) {
             throw new IllegalArgumentException("Duplicate alias \"" + alias + "\"");
         }
-        return map.put(alias, translation);    // ToDo: handle & test duplicate Aliases
     }
 
+    /**
+     * Returns matching translation, or null when no match found
+     * @param alias key to be found
+     * @return translation matching above key
+     */
     public Translation get(Alias alias) {
         return map.get(alias);
     }
 
+    /**
+     * Returns set of aliases from collection of rules
+     * @return Set{@literal <Alias>}
+     */
     public Set<Alias> getAliases() {
         return map.keySet();
     }
 
+    /**
+     * Loads rule definitions from external file
+     * @param file
+     * @throws IOException 
+     */
     public void loadFile(Path file) throws IOException {
         loadLines(Files.lines(file));
     }
 
-    public int loadLines(Stream<String> lines) {    // ToDo: test correct addition to Map
-        loadedLines = 0;
+    /** Loads rule as single line into collection 
+     * @param lines line containing rule definition
+     */
+    public void loadLines(Stream<String> lines) {    // ToDo: test correct addition to Map
         lines.forEach(l -> {
             add(l);
-            loadedLines++;
         });
-        return loadedLines;
-    }
-
-    private List<Rule> validate(String line) {
-        // skip comment lines
-        // skip blank lines
-        // skip empty lines
-        if (!line.matches(".+:.+")) {
-            throw new IllegalArgumentException("missing colon in line " + line);
-        }
-        String[] fields = line.split(":");
-        String translation = fields[0].trim();
-        String[] aliases = fields[1].split(",");
-
-        List<Rule> ruleList = new ArrayList<>();
-        for (String s : aliases) {
-            ruleList.add(new Rule(new Alias(s), new Translation(translation)));
-        }
-        return ruleList;
     }
 }
