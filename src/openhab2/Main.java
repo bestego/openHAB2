6de56aed6c0fd129e83b8a7462ac7623233a9fa4
erik <erik@192.168.0.58>
2019-08-20 18:09:45 +0200

    main method now takes directory pointing to external files, as an argument

--- src/openhab2/Main.java
+++ src/openhab2/Main.java
@@ -18,26 +18,43 @@ import java.nio.file.Paths;
 public class Main {
 
     public static void main(String... args) throws IOException {
-        Rules itemRules = new Rules();
-        itemRules.loadFile(Paths.get("items.txt"));
-        Rules commandRules = new Rules();
-        commandRules.loadFile(Paths.get("actions.txt"));
-        Rules valueRules = new Rules();
-        valueRules.loadFile(Paths.get("parameters.txt"));
         
+        String dir = "./";
+        if (args.length > 0 ) {
+            dir = args[0];
+            if ( ! dir.substring(dir.length()-1).matches("[\\/]") ) {
+                dir = dir + "/";
+            }
+        }
+
+        Rules itemRules = new Rules();
+//        When using resources        
+//        System.out.println(new Main().getClass().getResource("/items.txt"));     
+//        System.out.println("Current directory: " + System.getProperty("user.dir"));        
+//        itemRules.loadFile(Paths.get(new Main().getClass().getResource("/items.txt").toURI()));
+        itemRules.loadFile(Paths.get(dir + "items.txt"));
+        Rules commandRules = new Rules();
+        commandRules.loadFile(Paths.get(dir + "actions.txt"));
+        Rules valueRules = new Rules();
+        valueRules.loadFile(Paths.get(dir + "parameters.txt"));
+
         Translator itemXlate = new Translator(itemRules);
         Translator commandXlate = new Translator(commandRules);
-        Translator valueXlate= new Translator(valueRules);
+        Translator valueXlate = new Translator(valueRules);
 
         String line;
         try {
-            BufferedReader commands = newBufferedReader(Paths.get("commands.txt"), Charset.forName("ISO-8859-1"));
+            BufferedReader commands = newBufferedReader(Paths.get(dir + "commands.txt"), Charset.forName("ISO-8859-1"));
             while ((line = commands.readLine()) != null) {
-                
-                if ( line.matches("[ \t]*")) continue;              // skip empty|blank lines
-                if (line.matches("[ \t]*[#!].*")) continue;         // skip comment lines
-                System.out.println(line);                                            
-                
+
+                if (line.matches("[ \t]*")) {
+                    continue;              // skip empty|blank lines
+                }
+                if (line.matches("[ \t]*[#!].*")) {
+                    continue;         // skip comment lines
+                }
+                System.out.println(line);
+
                 System.out.println("=> " + itemXlate.translate(line) + ", " + commandXlate.translate(line) + ", " + valueXlate.translate(line));
                 System.out.println();
 
